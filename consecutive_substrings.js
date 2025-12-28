480758fc5e8af7e0e6b234f849a698e59e5a0513
Faizah <lolcanyouseeme@gmail.com>
2025-12-28 18:20:21 +0300

    Initial commit

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,5 +1,19 @@
 function consecutiveSubstrings(string) {
   // type your code here
+  //create an array to store all the substrings 
+   const result = [];
+
+  //Create a loop to go through all the substrings
+  for (let i = 0; i < string.length; i++) {
+    let current = "";
+
+    for (let j = i; j < string.length; j++) {
+      current += string[j];
+      result.push(current);
+    }
+  }
+
+  return result;
 }
 
 if (require.main === module) {
